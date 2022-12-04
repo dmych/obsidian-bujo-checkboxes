104c30c44d506b441d47d7dc072399dfbc18aecc
dmych <dbrechalov@gmail.com>
2022-12-04 14:32:43 +0300

    Added essential functionality

--- main.ts
+++ main.ts
@@ -2,80 +2,39 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface BujoCheckboxesSettings {
+    availableCheckboxes: '';
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const availableMarks = '/x-<>!?*"lbiSIpcfkwud';
+
+const DEFAULT_SETTINGS: BujoCheckboxesSettings = {
+    availableCheckboxes: availableMarks
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class BujoCheckboxes extends Plugin {
+	settings: BujoCheckboxesSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		    id: 'toggle-bujo-checkboxd',
+		    name: 'Toggle BuJo Checkbox',
+		    editorCallback: (editor: Editor, view: MarkdownView) => {
+			console.log('toggle bujo checkbox');
+			this.toggleTodos(editor, true);
+			},
+		    hotkeys: [
+			{
+			    modifiers: ["Mod"],
+			    key: ".",
+			},
+		    ],
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
 		});
+		this.addSettingTab(new BuJoSettingTab(this.app, this));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,28 +48,102 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+    getSelectedText(editor) {
+        if (editor.somethingSelected()) {
+            // Toggle to-dos under the selection
+            var cursorStart = editor.getCursor('from');
+            var cursorEnd = editor.getCursor('to');
+	    console.log('cursor: '+cursorStart+':'+cursorEnd);
+            var content = editor.getRange({ line: cursorStart.line, ch: 0 }, { line: cursorEnd.line, ch: editor.getLine(cursorEnd.line).length });
+	    console.log('selection: '+content);
+            return {
+                start: { line: cursorStart.line, ch: 0 },
+                end: {
+                    line: cursorEnd.line,
+                    ch: editor.getLine(cursorEnd.line).length,
+                },
+                content: content,
+            };
+        }
+        else {
+            // Toggle the todo in the line
+            var lineNr = editor.getCursor().line;
+            var contents = editor.getDoc().getLine(lineNr);
+            var cursorStart = {
+                line: lineNr,
+                ch: 0,
+            };
+            var cursorEnd = {
+                line: lineNr,
+                ch: contents.length,
+            };
+            var content = editor.getRange(cursorStart, cursorEnd);
+	    console.log('cursor: '+cursorStart+':'+cursorEnd+' content: '+content);
+            return { start: cursorStart, end: cursorEnd, content: content };
+        }
+    }
+    
+    doReplaceTodoElement(startText, optionList)
+    {
+	// var optionList = ' '+this.settings.availableCheckboxes;
+	console.log('replace: '+startText+' options: '+optionList);
+	if (startText === '') {
+	    startText = '- ';
+	}
+	else if (startText === '- ') {
+	    startText = '- [ ] '
+	}
+	else {
+	    optionList = ' ' + optionList;
+	    for (let i=0; i < optionList.length; i++) {
+		var next = (i+1) % optionList.length;
+		if (startText === "- [" + optionList[i] + "] ") {
+		    startText = '- [' + optionList[next] + '] ';
+		    break;
+		}
+	    }
+	}
+	console.log('new startText: "'+startText+'"');
+	return startText;
+    }
+    replaceTodoElement = (match, spaces, startText, sentence) => {
+	var replacement = this.doReplaceTodoElement(startText, this.settings.availableCheckboxes);
+	console.log('spaces: "'+spaces+'" startText: "'+startText+'" sentence: "'+sentence+'"');
+	console.log('replacement: '+replacement);
+	return spaces + replacement + sentence;
+    }
+
+    toggleTodos(editor, toggle) {
+        var re = /(^\s*|^\t*)(-\s\[ \]\s|-\s\[.\]\s|\*\s|-\s|\d*\.\s|\*\s|\b|^)([^\n\r]*)/gim;
+	console.log("toggle: "+toggle);
+	console.log("editor: "+editor);
+	var optionList = availableMarks;
+	if (toggle)
+	    optionList = this.settings.availableCheckboxes;
+	console.log("available list: "+optionList);
+        var selection = editor.somethingSelected();
+	console.log('selection: '+selection);
+        var selectedText = this.getSelectedText(editor);
+	console.log('selectedTest: ' +selectedText.content);
+        var newString = selectedText.content.replace(re, this.replaceTodoElement);
+        editor.replaceRange(newString, selectedText.start, selectedText.end);
+        // Keep cursor in the same place
+        if (selection) {
+            editor.setSelection(selectedText.start, {
+                line: selectedText.end.line,
+                ch: editor.getLine(selectedText.end.line).length,
+            });
+        }
+
+    }
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class BuJoSettingTab extends PluginSettingTab {
+	plugin: BujoCheckboxes;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: BujoCheckboxes) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +153,19 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
+	    containerEl.createEl('h2', {text: 'BuJo Checkboxes Settings'});
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Available Checkboxes')
+		.setDesc('')
+		.addText(text => text
+		    .setPlaceholder(availableMarks)
+		    .setValue(this.plugin.settings.availableCheckboxes)
+		    .onChange(async (value) => {
+			if (value === "")
+			    value = availableMarks;
+			console.log('checkboxes: ' + value);
+			this.plugin.settings.availableCheckboxes = value;
+			await this.plugin.saveSettings();
+		    }));
 	}
 }
